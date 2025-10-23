c548ab113786538db3ef7afb4c070e4adb429db1
ariellp17 <albujamolinaariel@gmail.com>
2025-10-22 20:33:46 -0500

    paso echo

--- ahorcadp/ahorcado.js
+++ ahorcadp/ahorcado.js
@@ -1,9 +1,21 @@
 
 esMayuscula = function (caracter) {
-    let  codigo = caracter.charCodeAt(0);
-    if (codigo >= 65 && codigo <= 90){
+    let codigo = caracter.charCodeAt(0);
+    if (codigo >= 65 && codigo <= 90) {
         return true
-    }else{
+    } else {
         return false
     }
 }
+
+guradarPalabra = function () {
+    let palabrasecreta = "";
+    palabrasecreta = document.getElementById("txtSecreta");
+    if (palabra.length !== 5) {
+        alert("Debe ingresar una palabra de 5 letras ");
+    } if (![A - Z].test(palabra)) {
+        alert("Debe ingresar una palabra de 5 letras mayúsculas");
+    }
+    palabrasecreta = palabra
+}
+
